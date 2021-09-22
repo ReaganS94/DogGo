6dd12a92b6f74b2cb04476e6dc9c4272cf9b5e91
Moldir28 <75260678+Moldir28@users.noreply.github.com>
2021-09-22 17:01:50 +0200

    added the image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {TiSocialFacebook, TiSocialInstagram, TiSocialTwitter, TiSocialPinterest} from 'react-icons/ti'; 
 import "./Footer.css"
 
+import dog from '../img/transparent-background/3.png'
+
 function Footer() {
     return (
     <div className="footer">
@@ -15,8 +17,8 @@ function Footer() {
              <p>Copyright &copy; 2021</p>
              <p>Terms and Conditions</p>
              <p>Privacy Policy</p>
+             <img src={dog} width="100px"/>
            </div>
-                
     </div>
     )
 }
