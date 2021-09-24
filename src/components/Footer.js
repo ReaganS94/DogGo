d3c2fbe32e2733ebea59a0a7c0482ff86760aa61
Moldir28 <75260678+Moldir28@users.noreply.github.com>
2021-09-24 15:58:11 +0200

    new footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,31 @@ import {TiSocialFacebook, TiSocialTwitter, TiSocialPinterest} from 'react-icons/
 import {AiOutlineInstagram} from 'react-icons/ai';
 import "./Footer.css"
 
-import dog from '../img/transparent-background/3.png'
-import logo from '../img/logo/doggo_ci_final_6.png'
+import dog from '../img/transparent-background/18.png'
+import logo from '../img/logo/logo_redyellow.png'
 
 function Footer() {
     return (
-    <div className="footer">
-           <img src={logo} className="logo" />
-           <div className="socialMediaIcons">
-             <TiSocialFacebook className="icons"/>
-             <AiOutlineInstagram className="icons"/>
-             <TiSocialTwitter className="icons"/>
-             <TiSocialPinterest className="icons"/>
-           </div>
-           <div className="footerContent">
+    <>
+     <div className="footer">
+       <div className="logo">
+         <img src={logo}/>  
+         <p>join the community</p>
+       </div>
+       <div className="socialMedia">
+             <TiSocialFacebook className="icon"/>
+             <AiOutlineInstagram className="icon"/>
+             <TiSocialTwitter className="icon"/>
+             <TiSocialPinterest className="icon"/> 
+       </div>
+       <div className="footerContent">
              <p>Copyright &copy; 2021</p>
              <p>Terms and Conditions</p>
              <p>Privacy Policy</p>
-           </div>
-           <img src={dog} className='footerKeeper'/>
+       </div>
     </div>
+    </>
+   
     )
 }
 
