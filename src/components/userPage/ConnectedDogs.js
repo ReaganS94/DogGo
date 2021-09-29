0ff5015077233cc70ebc1bb1bddf9816c22ea8be
Moldir28 <75260678+Moldir28@users.noreply.github.com>
2021-09-29 11:01:12 +0200

    added the pic of the dog

--- src/components/userPage/ConnectedDogs.js
+++ src/components/userPage/ConnectedDogs.js
@@ -6,8 +6,12 @@ function ConnectedDogs({dog}) {
  
     return (
       <div className="linkedDogs">
-       {dog.map((dog => <img src={dog.profilePic} />))};
+       <h4>my fluffy friends</h4>
+      <div className="dogPics">
+       {dog.map((dog) => (
+       <img src={dog.profilePic} />))}
       </div>
+    </div>
 
     )
   
