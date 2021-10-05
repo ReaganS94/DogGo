ebd4749d016b0860ec7e0f208938988851ae8b99
ReaganS <reagansasan@gmail.com>
2021-10-05 12:52:01 +0200

    added header with navbar

--- src/components/places/DisplayUsersMap.js
+++ src/components/places/DisplayUsersMap.js
@@ -4,6 +4,7 @@ import LocationMarker from "./LocationMarker";
 import LocationsList from "./LocationsList";
 import Search from "./Search";
 import axios from "axios";
+import PlacesHeader from "./PlacesHeader";
 
 function DisplayUsersMap() {
   const [userLocation, setUserLocation] = useState([]);
@@ -19,36 +20,43 @@ function DisplayUsersMap() {
   }, []);
   console.log(userLocation);
   return (
-    <div className="leaflet-container">
-      <MapContainer center={[52.52, 13.405]} zoom={12} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {userLocation &&
-          userLocation.map((users) => (
-            <Marker
-              key={users._id}
-              position={[users.LatLng.lat, users.LatLng.lng]}
-              riseOnHover={true}
-            >
-              <Popup>
-                <img
-                  src={users.profilePic}
-                  width="180px"
-                  height="200px"
-                  alt={users.username}
-                />{" "}
-                <br />
-                {users.username} <br />
-                {users.email} <br />
-                <br />
-                {users.about}
-              </Popup>
-            </Marker>
-          ))}
-        <Search />
-      </MapContainer>
+    <div>
+      <PlacesHeader />
+      <div className="leaflet-container">
+        <MapContainer
+          center={[52.52, 13.405]}
+          zoom={12}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {userLocation &&
+            userLocation.map((users) => (
+              <Marker
+                key={users._id}
+                position={[users.LatLng.lat, users.LatLng.lng]}
+                riseOnHover={true}
+              >
+                <Popup>
+                  <img
+                    src={users.profilePic}
+                    width="180px"
+                    height="200px"
+                    alt={users.username}
+                  />{" "}
+                  <br />
+                  {users.username} <br />
+                  {users.email} <br />
+                  <br />
+                  {users.about}
+                </Popup>
+              </Marker>
+            ))}
+          <Search />
+        </MapContainer>
+      </div>
     </div>
   );
 }
